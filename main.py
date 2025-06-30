f7fcc639ebca53dd0818ce053c0c1d6d7dd43cee
feczkob <botond.feczko@tngtech.com>
2025-06-30 21:22:21 +0200

    refactor main

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ SCRAPER_CLASS_MAP = {
     "mountex": MountexScraper,
 }
 
-with open('config/sites.yaml') as f:
-    config = yaml.safe_load(f)
-
-with open('config/categories.yaml') as f:
-    categories = yaml.safe_load(f)['categories']
+def load_config():
+    with open('config/sites.yaml') as f:
+        sites = yaml.safe_load(f)
+    with open('config/categories.yaml') as f:
+        categories = yaml.safe_load(f)['categories']
+    return sites, categories
 
 def print_discounts(site_name, discounts):
     if not discounts:
@@ -27,22 +28,30 @@ def print_discounts_category(category, site_name, discounts):
     for discount in discounts:
         print(f"- {discount['product']}\n  URL: {discount['url']}")
 
-scraper_instances = {}
-
-for site in config['sites']:
-    if not site.get('enabled', True):
-        continue
-    site_name = site['name']
-    scraper_class = SCRAPER_CLASS_MAP[site_name]
-    scraper = scraper_class()
-    scraper_instances[site_name] = scraper
-
-    discounts = scraper.check_discounts()
-    print_discounts(site_name, discounts)
-
-for category, urls in categories.items():
-    for site_name, scraper in scraper_instances.items():
-        if site_name not in urls:
+def instantiate_scrapers(sites):
+    scraper_instances = {}
+    for site in sites['sites']:
+        if not site.get('enabled', True):
             continue
-        discounts = scraper.extract_discounts_from_category(urls[site_name])
-        print_discounts_category(category, site_name, discounts)
+        site_name = site['name']
+        scraper_class = SCRAPER_CLASS_MAP[site_name]
+        scraper_instances[site_name] = scraper_class()
+    return scraper_instances
+
+def main():
+    sites, categories = load_config()
+    scraper_instances = instantiate_scrapers(sites)
+
+    for site_name, scraper in scraper_instances.items():
+        discounts = scraper.check_discounts()
+        print_discounts(site_name, discounts)
+
+    for category, urls in categories.items():
+        for site_name, scraper in scraper_instances.items():
+            if site_name not in urls:
+                continue
+            discounts = scraper.extract_discounts_from_category(urls[site_name])
+            print_discounts_category(category, site_name, discounts)
+
+if __name__ == "__main__":
+    main()
